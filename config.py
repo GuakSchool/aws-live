4d67ef446453dd737246e5012ea473282f43e42d
GuakSchool <155498075+GuakSchool@users.noreply.github.com>
2026-08-20 12:52:06 +0800

    Update database and bucket configuration

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-customhost = "database-1.chauwtk5goof.us-east-1.rds.amazonaws.com"
+customhost = "employee.cn7uueau2ci3.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "lowchoonkeat-employee1"
+custombucket = "wooijun-employee"
 customregion = "us-east-1"
 
